0913e4856997b2ddf73da5e6ac396fd0d8e974ad
tina1510 <141275553+tina1510@users.noreply.github.com>
2023-12-26 17:23:28 +0100

    1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,8 @@
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-import izitoast from "izitoast";
+import izitoast from 'izitoast';
 import "izitoast/dist/css/iziToast.min.css";
 
 
